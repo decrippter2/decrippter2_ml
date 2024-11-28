8c926a64048d5fadf12362ae398f9f03754bac9b
Roman Sanz <roman.sanzalchu@wur.nl>
2024-11-28 16:46:49 +0100

    for rewritting and validation of the new files, following schema

---
+++ json_refinery/json_v2_validations.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+
+def import_dict(json_file):
+    with open(json_file) as in_json:
+        json_dict = json.load(in_json)
+    return json_dict
+
+
+for _dirpath, _dirnames, filenames in os.walk(
+    "C:/Users/rsanz/PycharmProjects/decrippter2_ml/json_data_v2"
+):
+    for filename in filenames:
+        file = (
+            "C:/Users/rsanz/PycharmProjects/decrippter2_ml/json_data_v2"
+            + "/"
+            + filename
+        )
+        dictionary = import_dict(file)
+        bgc_number = dictionary["identifiers"]["mibig_acc"]
+        for entry in dictionary["entries"]:
+            if entry["leader"] == [] and entry["follower"] == []:
+                print("missing leader/follower in ", filename)
+            for core in entry["core"]:
+                if core in entry["leader"][0] and core != "TBA":
+                    print(
+                        "Core repetion in leader sequence of ",
+                        filename[:-5],
+                        " from BGC ",
+                        bgc_number,
+                    )
+                elif core in entry["follower"][0] and core != "TBA":
+                    print(
+                        "Core repetion in follower sequence of ",
+                        filename[:-5],
+                        " from BGC ",
+                        bgc_number,
+                    )
---
+++ json_refinery/rewrite_jsons_to_schema.py
@@ -0,0 +1,84 @@
+import json
+import os
+
+
+def write_json_dict(infolder, outfolder):
+    for _dirpath, _dirnames, filenames in os.walk(infolder):
+        file_count = 1
+        for filename in filenames:
+            out_dict = {
+                "identifiers": {"ncbi_acc": "", "mibig_acc": ""},
+                "compound_name": [],
+                "ripp_class": "",
+                "ref": [],
+                "entries": [],
+            }
+
+            with open(infolder + "/" + filename) as json_file:
+                json_dict = json.load(json_file)
+                json_name_split = filename.split("/")
+                json_name = json_name_split[-1]
+                out_dict["identifiers"]["ncbi_acc"] = json_dict["mibig_acc"][
+                    "accession"
+                ]
+                out_dict["identifiers"]["mibig_acc"] = json_name[:-5]
+                for compound in json_dict["mibig_acc"]["compound_name"]:
+                    out_dict["compound_name"].append(compound)
+                out_dict["ripp_class"] = json_dict["mibig_acc"]["ripp_class"]
+                out_dict["ref"].append(json_dict["mibig_acc"]["ref"])
+
+                for protein_id in json_dict["mibig_acc"]["protein_ids"]:
+                    entry_dict = {
+                        "protein_ids": {"genpept": "", "uniprot": ""},
+                        "complete": "",
+                        "leader": [],
+                        "core": [],
+                        "follower": [],
+                    }
+                    entry_dict["protein_ids"]["genpept"] = protein_id
+                    entry_dict["complete"] = json_dict["mibig_acc"][protein_id][
+                        "complete_seq"
+                    ]
+                    # check for entry duplicates
+                    """
+                    exists_dupl=False
+                    for key in json_dict["mibig_acc"]:
+                        if protein_id[:-2] in key:
+                            if "_" in key or ".2" in key:
+                                exists_dupl=True
+                        else:
+                            continue
+                    if exists_dupl==True:
+                        lead_list=[]
+                        fol_list=[]
+                        for key in json_dict["mibig_acc"]:
+                            if protein_id[:-2] in key:
+                                lead_list.append(json_dict["mibig_acc"][key]["leader_seq"])
+                        out_dict["entries"].append(entry_dict)
+                    else:
+                    """
+                    lead_list = []
+                    cor_list = []
+                    fol_list = []
+                    for key in json_dict["mibig_acc"]:
+                        if protein_id[:-2] in key:
+                            lead_list.append(json_dict["mibig_acc"][key]["leader_seq"])
+                            cor_list.append(json_dict["mibig_acc"][key]["core_seq"])
+                            fol_list.append(json_dict["mibig_acc"][key]["follower_seq"])
+                    shortest_lead = min(lead_list, key=len)
+                    shortest_follower = min(fol_list, key=len)
+
+                    entry_dict["leader"].append(shortest_lead)
+                    entry_dict["follower"].append(shortest_follower)
+                    entry_dict["core"].extend(cor_list)
+                    out_dict["entries"].append(entry_dict)
+            outfile_name = "ripp" + "0" * (7 - len(str(file_count))) + str(file_count)
+            with open(outfolder + "/" + outfile_name + ".json", "w") as outfile:
+                json.dump(out_dict, outfile, indent=6)
+            file_count += 1
+
+
+write_json_dict(
+    "C:/Users/rsanz/PycharmProjects/decrippter2_ml/json_data",
+    "C:/Users/rsanz/PycharmProjects/decrippter2_ml/json_data_v2",
+)
