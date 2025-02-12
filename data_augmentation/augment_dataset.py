32e0bcbeeb30d3afb52379c27c2d881756ca8030
Roman Sanz <roman.sanzalchu@wur.nl>
2025-02-12 09:31:03 +0100

    added eraser of duplicate samples

--- data_augmentation/augment_dataset.py
+++ data_augmentation/augment_dataset.py
@@ -19,20 +19,37 @@ class DataAugmentationManager(FeatureExtractor):
         blast_obj.jsons_to_fastas()
         blast_obj.run()
         for xml in blast_obj.ncbi_results.iterdir():
-            ripp_id=xml[:11]
+            blast_obj.extract_xml2(xml[:-2]) #all xmls file results are converted to multifasta files
 
-
-
-    def blast_subclasses(self):
-
+    def erase_duplicates(self,dataframe):
+        sequences = []
+        drop_indices = []
+        for index, row in dataframe[dataframe.validation == "yes"].iterrows():
+            print(index)
+            sequences.append(row["sequence"])
+        for index, row in dataframe[dataframe.validation == "no"].iterrows():
+            print(index)
+            if row["sequence"] not in sequences:
+                sequences.append(row["sequence"])
+            else:
+                drop_indices.append(index)
+        dataframe = dataframe.drop(drop_indices)
+        return dataframe
     def expand_dataset(self):
+        self.expansion_multifasta()
         #build validated dataset
-        positive_dataset=self.build_validated_dataset('zenodo_ripp.fa',True,True)
-        negative_dataset=self.build_validated_dataset('negative.fa',False,True)
+        positive_dataset=self.build_dataset('zenodo_ripp.fa',True,True)
+        negative_dataset=self.build_dataset('negative.fa',False,True)
         #record subclasses for blast results
 
         #build augmented fraction of dataset
+        blast_obj=BlastManager()
+        augmented_data=pd.DataFrame()
+        for fasta_file in blast_obj.ncbi_results_fasta.iterdir():
+            result_dataframe=self.build_dataset(fasta_file,True,False)
+            augmented_data=pd.concat([augmented_data,result_dataframe])
+        expanded_dataset=pd.concat([positive_dataset,negative_dataset,augmented_data])#merge all into big dataframe
+        return expanded_dataset #returns df as pd object
 
-        #merge all into big dataframe
 
 
